3cb47fec16848ec4b8569b4b0c3d9c156e7c34df
yara-png <yaracarvalhosantana0@gmail.com>
2025-02-07 14:08:09 -0300

    jogo da crobA

--- mobile-navbar.js
+++ mobile-navbar.js
@@ -269,29 +269,4 @@ function collision(head, array) {
     return false;
 }
 
-// Obtém o modal
-var modal = document.getElementById("meuModal");
-
-// Obtém o botão que abre o modal
-var btn = document.getElementById("abrirModal");
-
-// Obtém o elemento <span> que fecha o modal
-var span = document.getElementsByClassName("close")[0];
-
-// Quando o usuário clica no botão, abre o modal
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// Quando o usuário clica no <span> (x), fecha o modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// Quando o usuário clica fora do modal, fecha-o
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
 
